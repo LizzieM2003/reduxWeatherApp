f4ddccbdc80370e2df0bc2101b2b8bd31112b5a0
Liz Mendes <liz_mendes2001@yahoo.co.uk>
2017-02-28 14:07:00 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
