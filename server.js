752fb7865b56d3664a307ea02773ec75cccd24ef
Liz Mendes <liz_mendes2001@yahoo.co.uk>
2017-02-28 14:55:18 +0000

    Change server file to run on heroku

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const port = process.env.PORT || 8000;
 const app = express();
 
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static('public'));
 
 app.listen(port, () => {
