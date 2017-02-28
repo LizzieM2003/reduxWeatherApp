2cf8a209d74fc7252bd949dec7bc482fa4ec84a0
Liz Mendes <liz_mendes2001@yahoo.co.uk>
2017-02-28 16:26:02 +0000

    Environment variables setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-import { API_KEY } from '../../config';
+// import { API_KEY } from '../../config';
+const API_KEY = process.env.API_KEY;
+
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
